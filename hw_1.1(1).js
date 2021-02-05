38cea608f214169b705f1bcec536329f46661e2b
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-05 22:02:15 +0200

    Homework №1

--- hw_1.1(1).js
+++ hw_1.1(1).js
@@ -1,37 +1,39 @@
 /* Переменная хранит в себе значение от 0 до 9. 
 Написать скрипт который будет выводить слово “один”, если переменная хранит значение 1. 
 Выводить слово “два” - если переменная хранит значение 2, и т.д. для всех цифр от 0 до 9. 
-Реализовать двумя способами. 2-ой способ. */
+Реализовать двумя способами. 1-ый способ. */
 let num = prompt("Введите число: ");
-if(num == 1) {
-    alert("Один");
-}
-if(num == 2) {
-    alert("Два");
-}
-if(num == 3) {
-    alert("Три");
-}
-if(num == 4) {
-    alert("Четыре");
-}
-if(num == 5) {
-    alert("Пять");
-}
-if(num == 6) {
-    alert("Шесть");
-}
-if(num == 7) {
-    alert("Семь");
-}
-if(num == 8) {
-    alert("Восемь");
-}
-if(num == 9) {
-    alert("Девять");
-}
-if(num == 0) {
-    alert("Ноль");
-} else if (num > 9) {
-    alert("Введите число, которое входит в промежуток 0-9");
+switch(num) {
+case "1":
+    console.log("Один");
+    break;
+case "2":
+    console.log("Два");
+    break;
+case "3":
+    console.log("Три");
+    break;
+case "4":
+    console.log("Четыре");
+    break;
+case "5":
+    console.log("Пять");
+    break;
+case "6":
+    console.log("Шесть");
+    break;
+case "7":
+    console.log("Семь");
+    break;
+case "8":
+    console.log("Восемь");
+    break;
+case "9": 
+    console.log("Девять");
+    break;
+case "0":
+    console.log("Ноль");
+    break;
+default:
+    console.log("Введите число, которое входит в промежуток 0-9");
 }
