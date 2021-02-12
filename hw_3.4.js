c385b07131ac2097ac64da93594199dba4082964
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-12 12:30:02 +0200

    Update hw_3.4.js

--- hw_3.4.js
+++ hw_3.4.js
@@ -3,7 +3,7 @@ function SumOfMatrix(matrix1, matrix2) {
     let resultM = [];
     for (let i = 0; i < matrix1.length; i++)
      { resultM[i] = [];
-       for (let j = 0; j < matrix1[0].length; j++) {
+       for (let j = 0; j < matrix1[i].length; j++) {
        resultM[i][j] = matrix1[i][j] + matrix2[i][j];
        }
      }
