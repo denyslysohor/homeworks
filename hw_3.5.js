7f16072094efe3cd023a9b93b3f667edea3545dd
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-12 16:24:35 +0200

    Update hw_3.5.js

--- hw_3.5.js
+++ hw_3.5.js
@@ -1,8 +1,8 @@
-/* Найти номер столбца двумерного массива сумма которого является максимальной. */
+/* Найти номер столбца двумерного массива сумма которого является максимальной(Аналогично для строки). */
 let matrix = [
-    [6, 4, 10],
-    [6, 949, 0],
-    [10, -5, 0]
+    [6, 40, 0],
+    [67, 0, 0],
+    [0, -5, 0]
 ];
 let resultOfCol = 0;
 let sum = 0;
@@ -12,6 +12,20 @@ for(let i = 0; i < matrix[0].length; i++) {
          sum = sumOfCols;
          resultOfCol = i + 1;
      }
+
  }
- console.log("Hомер столбца с максимальной суммой - " + resultOfCol);
+let resultOfRow = 0;
+let sum1 = 0;
+for(let j = 0; j < matrix.length; j++) {
+let summer = (matrix[j].reduce((sum, current) => { 
+    return sum + current;
+}))
+ if(sum1 <= summer) {
+     sum1 = summer;
+     resultOfRow = j + 1;
+ }
+}
+console.log("Hомер строки с максимальной суммой - " + resultOfRow);
+console.log("Сумма - " + sum1);
+console.log("Hомер столбца с максимальной суммой - " + resultOfCol);
 console.log("Сумма - " + sum);
