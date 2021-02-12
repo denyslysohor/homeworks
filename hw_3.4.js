4e4c596f7d1420dc18652f5d6cfc6ea0288b6fd2
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-12 12:35:02 +0200

    Update hw_3.4.js

--- hw_3.4.js
+++ hw_3.4.js
@@ -1,15 +1,12 @@
 /* Написать функцию, которая суммирует две матрицы. */
 function SumOfMatrix(matrix1, matrix2) {
     let resultM = [];
-    for (let i = 0; i < matrix1.length; i++)
-     { resultM[i] = [];
+    for (let i = 0; i < matrix1.length; i++) { 
+       resultM[i] = [];
        for (let j = 0; j < matrix1[i].length; j++) {
-       resultM[i][j] = matrix1[i][j] + matrix2[i][j];
+        resultM[i][j] = matrix1[i][j] + matrix2[i][j];
        }
      }
     return resultM;
 }
-console.log(SumOfMatrix([[76, 45], 
-                       [45, 45]], 
-[[23, 54], 
-[54, 54]]));
+console.log(SumOfMatrix([[76, 45], [45, 45]], [[23, 54], [54, 54]]));
