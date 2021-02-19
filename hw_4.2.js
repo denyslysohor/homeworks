f503f06256267f90fb3cbcb4549a372f9ab906b0
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-19 10:38:38 +0200

    Update hw_4.2.js

--- hw_4.2.js
+++ hw_4.2.js
@@ -1,32 +1,31 @@
 function ArrayGenerator(neg, pos) {
     let arr = [];
-    let count = 0;
-    let count1 = 0;
+    let countNeg = 0; // Счетчик для отрицательных чисел.
+    let countPos = 0; // Счетчик для положительных чисел.
     let percentageNeg = 0;
     let percentagePos = 0;
-    let arrCount = pos + neg;
+    let arrCount = pos + neg; // pos - положительные числа, neg - отрицательные.
     let lim = arrCount + 1;
+    
     for(let k = 4; k > arr.length; k--) { 
     for(let i = 0; i < neg; i++) {
         arr.push(Math.round(Math.random() * -10 * lim));
-        count++;
+        countNeg++;
     }
-    percentageNeg = (count * 100) / k;
+    percentageNeg = (countNeg * 100) / k;
     console.log(percentageNeg + "%");
     for(let j = 0; j < pos; j++) {
         arr.push(Math.round(Math.random() * 10 * lim));
-            count1++;
+            countPos++;
     }
-    percentagePos = (count1 * 100) / k;
+    percentagePos = (countPos * 100) / k;
     console.log(percentagePos + "%");
 
     if((neg + pos) > k){
         return [];
         }
-
 }
-   return arr;
-    
+   return arr;    
 }
     console.log(ArrayGenerator(2, 2))
 
