d0967c46c3acbce09ebee91f48ab1115798f7856
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-19 10:36:26 +0200

    Update hw_4.1.js

--- hw_4.1.js
+++ hw_4.1.js
@@ -1,23 +1,22 @@
- let a = function ArrayGenerator(neg, pos) {
+function ArrayGenerator(neg, pos) {
     let arr = [];
-    let arrCount = pos + neg;
+    let arrCount = pos + neg; // pos - положительные числа, neg - отрицательные.
     let lim = arrCount + 1;
-    for(counter = 8; counter > arr.length; counter--) { 
+    for(let k = 8; k > arr.length; k--) { 
     for(let i = 0; i < neg; i++) {
         arr.push(Math.round(Math.random() * -10 * lim));
     }
     for(let j = 0; j < pos; j++) {
         arr.push(Math.round(Math.random() * 10 * lim));
     }
-    if(arr.length > counter){
+    if(arr.length > k){
         return [];
         }
 
 }
-    return arr
+    return arr;
 }
-console.log(a(4, 4));
-
+console.log(ArrayGenerator(4, 4));
 
 
 
