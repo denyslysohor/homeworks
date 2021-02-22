6e303bdea8aca5d1d2a0f91fb21c4436e1d86116
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-22 22:08:36 +0200

    Update hw_5.2.js

--- hw_5.2.js
+++ hw_5.2.js
@@ -1,3 +1,3 @@
 let array = [343, 33, 4, 34, 325];
-let result = array.every((x, i, a) => a.indexOf(x) == a.lastIndexOf(x));
+let result = array.every((item, i, arr) => arr.indexOf(item) == arr.lastIndexOf(item));
 console.log(result);
