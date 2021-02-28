2f530a573ff0387f3cba13c7ee91ea55f29b6eae
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-28 21:31:18 +0200

    Delete hw_6.2.js

--- hw_6.2.js
+++
@@ -1,3 +0,0 @@
-var countOfPhrases = document.querySelectorAll(".phrase").length;
-console.log(countOfPhrases + " - количетсво тегов с классом \"phrase\"");
-var displayer = document.querySelectorAll(".phrase").forEach(element => console.log(element));
