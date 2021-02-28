77f701bfb72b41cb6815b0955de5cff2027ddc69
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-02-28 21:30:58 +0200

    Delete hw_6.1.js

--- hw_6.1.js
+++
@@ -1,3 +0,0 @@
-var countOfLinks = document.querySelectorAll("a").length;
-console.log(countOfLinks + " - количество ссылок на странице");
-var displayer = document.querySelectorAll("a").forEach(element => console.log(element));
